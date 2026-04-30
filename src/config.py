23709a2e753ea25fdc546593e7419377595ba0dd
B1t0ne <hornid2010@mail.ru>
2026-04-30 20:53:59 +0500

    move db folders to api folder

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 
 SCRIPT_PATH = os.path.dirname(__file__)
 
-USERS_JSON_PATH = os.path.join(SCRIPT_PATH, 'db', 'users.json')
-TESTS_JSON_PATH = os.path.join(SCRIPT_PATH, 'db', 'tests.json')
+USERS_JSON_PATH = os.path.join(SCRIPT_PATH, 'api', 'db', 'users.json')
+TESTS_JSON_PATH = os.path.join(SCRIPT_PATH, 'api', 'db', 'tests.json')
 
 TEMPLATES_FOLDER = os.path.join(SCRIPT_PATH, 'templates')
